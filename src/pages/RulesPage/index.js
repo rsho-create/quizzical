b29a79f623de57863e4bfcf3a22cf000b1050cf3
Prishal <92685854+Prishal-git@users.noreply.github.com>
2022-05-16 17:46:33 +0100

    Landing page & navbar

---
+++ src/pages/RulesPage/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Navbar } from "../../components";
+
+const RulesPage = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+
+export default RulesPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,6 @@
-export { default as Search } from "./Search";
-export { default as NotFound } from "./NotFound";
+export { default as GamePage } from "./GamePage";
+export { default as LandingPage } from "./LandingPage";
+export { default as NotFoundPage } from "./NotFoundPage";
+export { default as ResultsPage } from "./ResultsPage";
+export { default as RulesPage } from "./RulesPage";
+export { default as SettingsPage } from "./SettingsPage";
