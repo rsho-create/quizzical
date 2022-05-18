88255a8ee6dedcb2155c11f194adccadd62727e6
Prishal <92685854+Prishal-git@users.noreply.github.com>
2022-05-18 17:57:08 +0100

    Updating styling on setting and rules page

--- api/app.js
+++ api/app.js
@@ -1,21 +1,17 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const usersRoutes = require('./routes/users');
+const usersRoutes = require("./routes/users");
 const logRoutes = require("./middleware/log-routes");
 
-
 //middleware
-app.use(logRoutes)
+app.use(logRoutes);
 
-
-app.get('/', (req, res) => res.send('Welcome to EnterQuiz!'));
-app.use('/users', usersRoutes);
-
-
+app.get("/", (req, res) => res.send("Welcome to Quizzical!"));
+app.use("/users", usersRoutes);
 
 module.exports = app;
