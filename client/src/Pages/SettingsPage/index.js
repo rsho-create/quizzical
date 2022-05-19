1b83a1a7e205170f378556f72960c3c447686103
Prishal <92685854+Prishal-git@users.noreply.github.com>
2022-05-19 13:32:43 +0100

    added styling from dev branch to this branch for settings and rules page

--- client/src/Pages/SettingsPage/index.js
+++ client/src/Pages/SettingsPage/index.js
@@ -1,18 +1,36 @@
-import { Box } from '@mui/system';
+import { Box } from "@mui/system";
 import React, { useEffect, useState } from "react";
 import { Navbar } from "../../components";
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import InputAdornment from '@mui/material/InputAdornment';
-import AccountCircle from '@mui/icons-material/AccountCircle';
+import InputAdornment from "@mui/material/InputAdornment";
+import AccountCircle from "@mui/icons-material/AccountCircle";
 import { gameId, updateRoundSettings } from "../../reducers/gameSlice";
 import { fetchCategories, categories } from "../../reducers/categoriesSlice";
 import { fetchQuestions } from "../../reducers/questionsSlice";
-import { FormControl, InputLabel, MenuItem, Button, Select, CircularProgress, Typography, TextField, Container } from '@mui/material';
-import { Categories, SelectField, difficultyOptions, TextFieldComp, timerOptions, questionsOptions, settingsButtonStyles } from '../../components';
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Button,
+  Select,
+  CircularProgress,
+  Typography,
+  TextField,
+  Container,
+} from "@mui/material";
+import {
+  Categories,
+  SelectField,
+  difficultyOptions,
+  TextFieldComp,
+  timerOptions,
+  questionsOptions,
+  settingsButtonStyles,
+} from "../../components";
 
 const SettingsPage = () => {
-  // hooks 
+  // hooks
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
@@ -23,23 +41,23 @@ const SettingsPage = () => {
     numOfQuestions: 10,
     timer: 30,
     player1: "",
-    player2: ""
-  })
+    player2: "",
+  });
 
-  // each time something changes in the form it will find the key by name and set the value 
-  const changeHandler = e => {
-    setFormValue({...form, [e.target.name]: e.target.value})
- }
+  // each time something changes in the form it will find the key by name and set the value
+  const changeHandler = (e) => {
+    setFormValue({ ...form, [e.target.name]: e.target.value });
+  };
 
   // form input values
   function handleSubmit(e) {
     return e.preventDefault();
-  };
+  }
 
   //getting categories from redux
   const allCategories = useSelector(categories);
   const categoriesSliced = allCategories.slice(24);
-  const categoryStatus = useSelector(state => state.categories.status);
+  const categoryStatus = useSelector((state) => state.categories.status);
 
   // fetching categories and loading form
   useEffect(() => {
@@ -50,20 +68,19 @@ const SettingsPage = () => {
     if (categoryStatus === "loading") {
       <Box mt={20}>
         <CircularProgress size={150} />
-      </Box>
+      </Box>;
     }
 
     if (categoryStatus === "failed") {
-        <Typography variant="h6" mt={20} color="red">
-          Technical Difficulties! Refresh the Page and Take a Shot!
-        </Typography>
-    };
+      <Typography variant="h6" mt={20} color="red">
+        Technical Difficulties! Refresh the Page and Take a Shot!
+      </Typography>;
+    }
   }, [categoryStatus, dispatch]);
-  
 
   const startGame = () => {
     dispatch(updateRoundSettings(form));
-    dispatch(fetchQuestions(form))
+    dispatch(fetchQuestions(form));
     navigate("/game");
   };
 
@@ -71,79 +88,94 @@ const SettingsPage = () => {
     <>
       <Navbar />
 
-      <Container maxWidth="md" style={{backgroundColor:"white"}}>
-
-        <Box textAlign="center" mt={5} >
-
-          <h1 className="quiz-settings">Quiz Settings</h1>
-          {"\n"}
-          {"\n"}
-          <h4 className="rules">How to Play</h4>
+      <Container maxWidth="md" style={{ backgroundColor: "white" }}>
+        <Box className="settings-container">
+          <h1 className="quiz-settings-title">Quiz Settings</h1>
+          <h4
+            className="quiz-settings-rules-link"
+            onClick={() => navigate("/rules")}
+          >
+            How to Play
+          </h4>
 
           <form onSubmit={handleSubmit}>
+            <Box mt={3} width="100%">
+              <FormControl fullWidth>
+                <InputLabel>Categories</InputLabel>
+                <Select
+                  value={form.category}
+                  name="category"
+                  label="Categories"
+                  onChange={changeHandler}
+                >
+                  {categoriesSliced.map(({ name }, id) => (
+                    <MenuItem value={id} key={id}>
+                      {name}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Box>
 
-          <Box mt={3} width="100%">
-            <FormControl  fullWidth >
-              <InputLabel>Categories</InputLabel>
-              <Select value={form.category} name="category" label="Categories" 
-              onChange={changeHandler} >
-
-                {categoriesSliced.map(({name}, id) => (
-                  <MenuItem value={id} key={id}>{name}</MenuItem>
-                ))}
-
-              </Select>
-            </FormControl>
-          </Box>
-            
-          <Box mt={3} width="100%">
-              <FormControl  fullWidth  >
+            <Box mt={3} width="100%">
+              <FormControl fullWidth>
                 <InputLabel>Difficulty</InputLabel>
-                <Select value={form.difficulty} name="difficulty" label="Difficulty" 
-                onChange={changeHandler}>
+                <Select
+                  value={form.difficulty}
+                  name="difficulty"
+                  label="Difficulty"
+                  onChange={changeHandler}
+                >
                   {difficultyOptions.map(({ id, name }) => (
-                      <MenuItem value={id} key={id}>
-                        {name}
-                      </MenuItem>
-                    ))}
+                    <MenuItem value={id} key={id}>
+                      {name}
+                    </MenuItem>
+                  ))}
                 </Select>
               </FormControl>
-          </Box>
+            </Box>
 
-          <Box mt={3} width="100%">
-              <FormControl  fullWidth  >
+            <Box mt={3} width="100%">
+              <FormControl fullWidth>
                 <InputLabel>Total Questions</InputLabel>
-                <Select value={form.numOfQuestions} name="numOfQuestions" label="Total Questions" 
-                onChange={changeHandler}>
+                <Select
+                  value={form.numOfQuestions}
+                  name="numOfQuestions"
+                  label="Total Questions"
+                  onChange={changeHandler}
+                >
                   {questionsOptions.map(({ id, name }) => (
-                      <MenuItem value={id} key={id}>
-                        {name}
-                      </MenuItem>
-                    ))}
+                    <MenuItem value={id} key={id}>
+                      {name}
+                    </MenuItem>
+                  ))}
                 </Select>
               </FormControl>
-          </Box>
+            </Box>
 
-          <Box mt={3} width="100%">
-              <FormControl  fullWidth >
+            <Box mt={3} width="100%">
+              <FormControl fullWidth>
                 <InputLabel>Timer</InputLabel>
-                <Select value={form.timer} name="timer" label="Timer" 
-                onChange={changeHandler}>
+                <Select
+                  value={form.timer}
+                  name="timer"
+                  label="Timer"
+                  onChange={changeHandler}
+                >
                   {timerOptions.map(({ id, name }) => (
-                      <MenuItem value={id} key={id}>
-                        {name}
-                      </MenuItem>
-                    ))}
+                    <MenuItem value={id} key={id}>
+                      {name}
+                    </MenuItem>
+                  ))}
                 </Select>
               </FormControl>
-          </Box>
+            </Box>
 
-          <Box mt={3} width="100%">
-            <FormControl fullWidth>
+            <Box mt={3} width="100%">
+              <FormControl fullWidth>
                 <TextField
                   variant="outlined"
-                  type='text'
-
+                  type="text"
                   label="Player 1 Name"
                   name="player1"
                   onChange={changeHandler}
@@ -155,15 +187,14 @@ const SettingsPage = () => {
                     ),
                   }}
                 />
-            </FormControl>
-        </Box>
+              </FormControl>
+            </Box>
 
-        <Box mt={3} width="100%">
-            <FormControl fullWidth>
+            <Box mt={3} width="100%">
+              <FormControl fullWidth>
                 <TextField
                   variant="outlined"
-                  type='text'
-
+                  type="text"
                   label="Player 2 Name"
                   name="player2"
                   onChange={changeHandler}
@@ -175,19 +206,23 @@ const SettingsPage = () => {
                     ),
                   }}
                 />
-            </FormControl>
-        </Box>
-
-            <Button fullWidth variant="contained" type="submit" 
-            style={settingsButtonStyles} onClick={startGame}> 
-              Play
-            </Button>
-
+              </FormControl>
+            </Box>
+            <Box style={{ justifyContent: "center", display: "flex" }}>
+              <Button
+                fullWidth
+                variant="contained"
+                type="submit"
+                style={settingsButtonStyles}
+                onClick={startGame}
+                className="primary-button"
+              >
+                Play
+              </Button>
+            </Box>
           </form>
-
         </Box>
       </Container>
-    
     </>
   );
 };
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,65 +4,65 @@ export { default as Results } from "./Results";
 export { default as Timer } from "./Timer";
 
 export var settingsButtonStyles = {
-    color: "#000a3c",
-    height: "80px",
-    fontWeight: 700,
-    display: "block",
-    border: "none",
-    fontSize: "30px",
-    cursor: "pointer",
-    marginTop: "30px",
-    backgroundColor: "#ffc107",
-    fontFamily: "Poppins",
-    textTransform: "none"
-}
+  color: "#000a3c",
+  // height: "80px",
+  fontWeight: 700,
+  display: "block",
+  border: "none",
+  // fontSize: "30px",
+  cursor: "pointer",
+  // marginTop: "30px",
+  backgroundColor: "#ffc107",
+  fontFamily: "Poppins",
+  textTransform: "none",
+};
 
 export var yellowStyles = {
-    color: "#000a3c",
-    height: "80px",
-    fontWeight: 700,
-    display: "block",
-    border: "none",
-    fontSize: "30px",
-    cursor: "pointer",
-    marginTop: "30px",
-    backgroundColor: "#ffc107",
-    fontFamily: "Poppins",
-    textTransform: "none",
-    width: "60%"
-}
+  color: "#000a3c",
+  height: "80px",
+  fontWeight: 700,
+  display: "block",
+  border: "none",
+  fontSize: "30px",
+  cursor: "pointer",
+  marginTop: "30px",
+  backgroundColor: "#ffc107",
+  fontFamily: "Poppins",
+  textTransform: "none",
+  width: "60%",
+};
 
 export var whiteStyles = {
-    color: "#000a3c",
-    height: "80px",
-    fontWeight: 700,
-    display: "block",
-    border: "none",
-    fontSize: "30px",
-    cursor: "pointer",
-    marginTop: "30px",
-    backgroundColor: "#e5e5e5",
-    fontFamily: "Poppins",
-    textTransform: "none",
-    width: "60%"
-}
+  color: "#000a3c",
+  height: "80px",
+  fontWeight: 700,
+  display: "block",
+  border: "none",
+  fontSize: "30px",
+  cursor: "pointer",
+  marginTop: "30px",
+  backgroundColor: "#e5e5e5",
+  fontFamily: "Poppins",
+  textTransform: "none",
+  width: "60%",
+};
 
 export const difficultyOptions = [
-    { id: "easy", name: "Easy"},
-    { id: "medium", name: "Medium"},
-    { id: "hard", name: "Hard"},
+  { id: "easy", name: "Easy" },
+  { id: "medium", name: "Medium" },
+  { id: "hard", name: "Hard" },
 ];
 
 export const timerOptions = [
-    { id: "15", name: 15 },
-    { id: "30", name: 30},
-    { id: "45", name: 45},
-    { id: "60", name: 60}
+  { id: "15", name: 15 },
+  { id: "30", name: 30 },
+  { id: "45", name: 45 },
+  { id: "60", name: 60 },
 ];
 
 export const questionsOptions = [
-    { id: "5", name: 5 },
-    { id: "10", name: 10 },
-    { id: "15", name: 15 },
-    { id: "20", name: 20 },
+  { id: "5", name: 5 },
+  { id: "10", name: 10 },
+  { id: "15", name: 15 },
+  { id: "20", name: 20 },
 ];
